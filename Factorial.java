2839133058d957231585fd26def3a6836cd394b2
Megha-kp <91820220+Megha-kp@users.noreply.github.com>
2021-10-03 12:56:10 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+import java.util.Iterator;
+
+public class Factorial {
+public static void main(String[] args) {
+	int number=5;
+	int fact=1;
+	for (int i= 1;i<=5;i++) {
+		
+		fact= fact*i;
+		
+	}
+	System.out.println("factorial is " + fact);
+	
+}
+
+
+}
